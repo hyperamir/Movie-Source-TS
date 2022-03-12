9fede54bc75a827e2461a22191f81d601318db56
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-12 13:51:56 -0500

    change Thumb component to ts

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Image } from "./Thumb.styles";
 import { Link } from "react-router-dom";
 
-const Thumb = (props) => {
+type Props = {
+  clickable: boolean;
+  movieId: number;
+  image: string
+}
+
+const Thumb: React.FC<Props> = (props) => {
   return (
     <div>
       {props.clickable ?
